067c40a14ae9023b262ebf093c292918f0efec3c
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-10-11 23:47:50 +0530

    my first commit

---
+++ Day 1/problem3.js
@@ -0,0 +1,8 @@
+//Problem 3 : Given and character if it is a consonant print "Consonant"
+
+let char = "b";
+ if(char=="b"||char=="c"||char=="d"||char=="f"){
+     console.log("consonant");
+ }else{
+   console.log("other");
+ }
---
+++ Day 1/problem4.js
@@ -0,0 +1,13 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a=10;
+let b=20;
+let c=19;
+
+if(a>b && a>c){
+  console.log("a is greatest");
+}else if(b>c && b>a){
+  console.log("b is greatest");
+}else{
+  console.log("c is greatest");
+}
